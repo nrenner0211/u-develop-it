f2beeae20cb1701aa674f030ef55f1f9c0ccd289
nrenner <nrenner0211@gmail.com>
2022-07-18 20:52:56 -0400

    routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
--- this will densure you start with a clean slate by deleting existing tables when file is ran
+-- this will ensure you start with a clean slate by deleting existing tables when file is ran
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -15,4 +16,12 @@ CREATE TABLE candidates (
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
